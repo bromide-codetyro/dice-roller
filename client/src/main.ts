868dc5bb7c3888968cbe219e9b9e912144c5822d
Chris <someguy@nowhere.dontlookforme.com>
2025-02-02 11:00:33 -0600

    prime on top

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { PrimeComponent } from './app/prime.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(PrimeComponent, appConfig)
   .catch((err) => console.error(err));

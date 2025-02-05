32e09c172cea831479723d0b2ae7687876025b83
chris <noone@nowhere.com>
2025-02-05 10:08:00 -0600

    Add cors and option for https

--- server/app.js
+++ server/app.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('client'));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// const options = {
+//   key: fs.readFileSync('key.pem'),
+//   cert: fs.readFileSync('cert.pem')
+// };
+//
+// https.createServer(options, app).listen(port, () =>
+//   console.log(`HTTPS server running on port ${port}`)
+// );
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
